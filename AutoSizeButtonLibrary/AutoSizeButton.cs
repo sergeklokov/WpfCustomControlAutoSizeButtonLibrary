c50a08fb9cba6ada9dc230d42720037cad9cec34
sergeklokov <serge.klokov@gmail.com>
2025-11-07 22:28:51 -0700

    changing to .net 4.8.1

--- AutoSizeButtonLibrary/AutoSizeButton.cs
+++ AutoSizeButtonLibrary/AutoSizeButton.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,11 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 
-namespace WpfCustomControlAutoSizeButtonLibrary
+namespace AutoSizeButtonLibrary
 {
     /// <summary>
     /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
@@ -21,14 +23,14 @@ namespace WpfCustomControlAutoSizeButtonLibrary
     /// Add this XmlNamespace attribute to the root element of the markup file where it is 
     /// to be used:
     ///
-    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControlAutoSizeButtonLibrary"
+    ///     xmlns:MyNamespace="clr-namespace:AutoSizeButtonLibrary"
     ///
     ///
     /// Step 1b) Using this custom control in a XAML file that exists in a different project.
     /// Add this XmlNamespace attribute to the root element of the markup file where it is 
     /// to be used:
     ///
-    ///     xmlns:MyNamespace="clr-namespace:WpfCustomControlAutoSizeButtonLibrary;assembly=WpfCustomControlAutoSizeButtonLibrary"
+    ///     xmlns:MyNamespace="clr-namespace:AutoSizeButtonLibrary;assembly=AutoSizeButtonLibrary"
     ///
     /// You will also need to add a project reference from the project where the XAML file lives
     /// to this project and Rebuild to avoid compilation errors:
@@ -43,6 +45,7 @@ namespace WpfCustomControlAutoSizeButtonLibrary
     ///     <MyNamespace:CustomControl1/>
     ///
     /// </summary>
+    /// 
 
     // The AutoSizeButton control implements a button
     // with a custom design-time experience. 
--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,6 @@
 # WPF Demo App Debugging WPF Custom Controls at Design Time, based on Microsoft's 2010 Walkthrough: 
 
-I'm rewriting it with .Net 8.0 c# WPF
-
 [Walkthrough: Debugging WPF Custom Controls at Design Time](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2010/bb514636(v=vs.100)?source=docs)
+
+Currently I use .Net Framework 4.8.1 for it, later I'm planning to rewrite it with .Net 8.0
 
